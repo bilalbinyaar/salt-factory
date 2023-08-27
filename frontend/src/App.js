22bce2c1359114cfa94f2c87e7029ba514b80fa3
Bilal Y <bilal.y@boomdc.local>
2023-08-27 18:39:35 +0300

    Router Fix Try

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './screens/Home';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes  } from 'react-router-dom';
 import About from './screens/About';
 
 function App() {
   return (
     <React.Fragment>
-      <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about-us" element={<About />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about-us" element={<About />} />
         </Routes>
         <Footer />
-      </Router>
     </React.Fragment>
   );
 }
