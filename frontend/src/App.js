a4ecf62387bca78159ddfa9f7a29be8d9f9b4fb9
bilal-bin-yar <bilalbinyar@neurog.ai>
2023-08-29 14:23:22 +0500

    Fixing the Routing 404

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './screens/Home';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './screens/About';
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about-us" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<About />} />
         </Routes>
         <Footer />
       </Router>
